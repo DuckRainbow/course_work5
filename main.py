9447292778e03c6d83b1e0fcb3870f66855b7c37
DuckRainbow <46005901+DuckRainbow@users.noreply.github.com>
2024-04-10 09:27:29 +0500

    Update main.py

--- main.py
+++ main.py
@@ -19,42 +19,45 @@ def main():
         '1122462'  # Skyeng
     ]
     params = config()
-
     new_list_com = []
     new_list_vac = []
 
     get_vac_data('http://api.hh.ru/vacancies', companies_ids)
-    create_database('hhru', params)
-    save_data_to_database(new_list_vac, new_list_com, 'hhru', params)
-
-    dbmanager = DBManager('hhru')
-    print('Выберите опцию:')
-    print('1. Вывести список всех компаний и количество вакансий у них.')
-    print('2. Вывести список всех вакансий с указанием названия компании.')
-    print('3. Вывести среднюю зарплату по вакансиям.')
-    print('4. Вывести список всех вакансий, у которых зарплата выше средней.')
-    print('5. Вывести список всех вакансий по слову.')
-    print('Введите любое число для того, чтобы завершить')
-    while True:
-        user_answer = input()
-        if int(user_answer) == 1:
-            data = dbmanager.get_companies_and_vacancies_count(params)
-            print(data)
-        elif int(user_answer) == 2:
-            data = dbmanager.get_all_vacancies(params)
-            print(data)
-        elif int(user_answer) == 3:
-            data = dbmanager.get_avg_salary(params)
-            print(data)
-        elif int(user_answer) == 4:
-            data = dbmanager.get_vacancies_with_higher_salary(params)
-            print(data)
-        elif int(user_answer) == 5:
-            user_word = input('Введите слово, по которому будем искать вакансии: ')
-            data = dbmanager.get_vacancies_with_keyword(params, user_word)
-            print(data)
-        else:
-            break
+    create_database('hhru')
+    save_data_to_database(new_list_vac, new_list_com, 'hhru')
+    conn = psycopg2.connect(dbname='hhru', params)
+    conn.autocommit = True
+    with conn.cursor() as cur:
+        dbmanager = DBManager('hhru')
+        print('Выберите опцию:')
+        print('1. Вывести список всех компаний и количество вакансий у них.')
+        print('2. Вывести список всех вакансий с указанием названия компании.')
+        print('3. Вывести среднюю зарплату по вакансиям.')
+        print('4. Вывести список всех вакансий, у которых зарплата выше средней.')
+        print('5. Вывести список всех вакансий по слову.')
+        print('Введите любое число для того, чтобы завершить')
+        while True:
+            user_answer = input()
+            if int(user_answer) == 1:
+                data = dbmanager.get_companies_and_vacancies_count()
+                print(data)
+            elif int(user_answer) == 2:
+                data = dbmanager.get_all_vacancies()
+                print(data)
+            elif int(user_answer) == 3:
+                data = dbmanager.get_avg_salary()
+                print(data)
+            elif int(user_answer) == 4:
+                data = dbmanager.get_vacancies_with_higher_salary()
+                print(data)
+            elif int(user_answer) == 5:
+                user_word = input('Введите слово, по которому будем искать вакансии: ')
+                data = dbmanager.get_vacancies_with_keyword(user_word)
+                print(data)
+            else:
+                break
+        conn.commit()
+        conn.close()
 
 
 if __name__ == '__main__':
